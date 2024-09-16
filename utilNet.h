211b7986d6a87c308c739656b905a2ad09767832
Toshio119 <toshiojp119@gmail.com>
2024-09-16 15:35:11 +0530

    Add files via upload

---
+++ utilNet.h
@@ -0,0 +1,27 @@
+#ifndef UTILNET_H
+#define UTILNET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE 1024
+
+int create_tcp_socket();
+int create_udp_socket();
+int bind_socket(int sockfd, const char *address, int port);
+int listen_socket(int sockfd, int backlog);
+int accept_connection(int sockfd, struct sockaddr_in *client_addr);
+int connect_to_server(int sockfd, const char *address, int port);
+ssize_t send_data(int sockfd, const void *data, size_t size);
+ssize_t receive_data(int sockfd, void *buffer, size_t size);
+void close_socket(int sockfd);
+int resolve_hostname(const char *hostname, char *ip_address, size_t size);
+
+#endif // UTILNET_H
