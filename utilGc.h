a11eeab7123b357aaf515dd777e4bc18658d9a95
Toshio119 <toshiojp119@gmail.com>
2024-10-05 13:06:01 +0530

    Update utilGc.h

--- utilGc.h
+++ utilGc.h
@@ -1,22 +1,4 @@
 /*
- * 
- * Usage of these utilities is intended to enhance memory management and debugging processes. The functions provided in this header file include:
- * - `track_malloc(size_t size)`: Allocates memory of the specified size and tracks the allocation.
- * - `track_calloc(size_t num, size_t size)`: Allocates memory for an array of elements, initializing them to zero, and tracks the allocation.
- * - `track_free(void *ptr)`: Frees previously allocated memory and removes the tracking information.
- * - `report_memory_leaks(void)`: Reports memory leaks detected at program exit.
- * - `report_memory_usage(void)`: Reports the total amount of memory currently allocated.
- , including total allocated memory, total number of allocations, and maximum memory usage. 
- * 
- * The use of `malloc`, `calloc`, and `free` macros redirects memory management calls to the tracking functions, allowing for automated monitoring and debugging. 
- * 
- * This tool is designed to assist in debugging and ensuring proper memory management. 
- * However, it is essential to perform additional checks and tests in production environments. 
- * Users should review the implementation of this code to ensure it meets the specific needs and security requirements of their application. 
- * (c) 2024 Toshio Nayashima
- * All rights reserved.
- */
-/*
 ===================================================
        U!nisoft Libarary's 
        Utility at it's peak
@@ -40,11 +22,11 @@ For support, contact: toshiojp119@gmail.com
  * Usage of these utilities is intended to improve memory management and debugging processes. The functions provided in this header file:
  * - `track_malloc(size_t size)`: Allocates memory of the specified size and tracks the allocation.
  * - `track_free(void *ptr)`: Frees previously allocated memory and removes the tracking information.
- * - `report_memory_leaks(void)`: Reports memory leaks detected at program exit.
+ * - `collect_all(void)`: frees all memory at program exit.
  * - `report_memory_usage(void)`: Reports the total amount of memory currently allocated.
  * - `track_fopen(const char *filename, const char *mode)`: Opens a file and tracks the file pointer.
  * - `track_fclose(FILE *file)`: Closes a previously opened file and removes the tracking information.
- * - `report_open_files(void)`: Reports any open file pointers detected at program exit.
+ * - `close_allfiles(void)`: frees all files at program exit..
  * 
  * The use of `malloc`, `free`, `fopen`, and `fclose` macros redirects memory management calls to the tracking functions, allowing for automated monitoring.
  * 
